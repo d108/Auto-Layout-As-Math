2865d50d7c410b89e0800390207b22b8829b32c5
Daniel Zhang (張道博) <dz1111@ikiapps.com>
2014-07-04 15:26:03 -1000

    Initial Commit

---
+++ Swift Auto Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Auto Layout
+//
+//  Created by Daniel Zhang (張道博) on 7/4/14.
+//  Copyright (c) 2014 Daniel Zhang (張道博). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
